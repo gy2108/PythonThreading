ced55e2bb886146b93c6c25c6d1a3705414756cc
gy2108 <gopalyadav2108@gmail.com>
2019-09-21 01:35:36 +0530

    Looping multiple threads

--- threading_Program.py
+++ threading_Program.py
@@ -3,18 +3,21 @@ import time
 
 start = time.perf_counter()
 
-def do_something():
-    print("Sleeping for 1 second")
-    time.sleep(1)
+def do_something(seconds):
+    print(f"Sleeping for {seconds} second")
+    time.sleep(seconds)
     print("Done Sleeping")
 
-t1 = threading.Thread(target=do_something)
-t2 = threading.Thread(target=do_something)
+threads = []
 
-t1.start()
-t2.start()
-t1.join()
-t2.join()
+for _ in range(10):
+    t = threading.Thread(target= do_something, args=[1.5])
+    t.start()
+    threads.append(t)
+
+for thread in threads:
+    thread.join()
+
 
 finish = time.perf_counter()
 answer = round(finish-start, 2)
